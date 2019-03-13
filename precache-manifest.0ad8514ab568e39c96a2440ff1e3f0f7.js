2f1d6abdd3b8ab92e976a499347d4a853cb5f898
Damian Soria <damian.soria.web@gmail.com>
2019-03-13 11:36:46 -0300

    Updates

---
+++ precache-manifest.0ad8514ab568e39c96a2440ff1e3f0f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bfe9edc4fef8d1efe3a",
+    "url": "/react-gh-pages/static/css/main.753d44f9.chunk.css"
+  },
+  {
+    "revision": "2bfe9edc4fef8d1efe3a",
+    "url": "/react-gh-pages/static/js/main.e5deb57f.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react-gh-pages/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0af641798ed93f3e94c7dbca09e7a035",
+    "url": "/react-gh-pages/index.html"
+  }
+];
